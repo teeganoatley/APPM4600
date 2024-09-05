9f20527a4d120142fd526937a78c33ab9443b877
Matthew Cielaszyk <teoa3083@colorado.edu>
2024-09-05 10:27:52 -0600

    added directories from lab 1

---
+++ Labs/Lab1/Lab1.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = [1,2,3]
+
+X = np.linspace(0, 2 * np.pi, 100)
+Ya = np.sin(X)
+Yb = np.cos(X)
+
+plt.plot(X, Ya)
+plt.plot(X, Yb)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
+# Exersizes 
+x = np.linspace(0, 10, 10)
+y = np.arange(10, 20, 1)
+
+first3x = x[:3]
+print('the first three entries of x are', first3x)
+
+w = 10**(-np.linspace(1,10,10))
+x = np.linspace(0,len(w),10)
+s = 3*w
+
+plt.semilogy(x,w)
+plt.semilogy(x,s)
+plt.xlabel('x')
+plt.ylabel('w, s')
+plt.show()
