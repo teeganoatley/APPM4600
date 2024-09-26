34004368503c2ef1e874661d36b01f16bf0ac252
teeganoatley <teoa3083@colorado.edu>
2024-09-26 10:58:41 -0600

    lab 5

--- Labs/Lab5/Lab5.py
+++ Labs/Lab5/Lab5.py
@@ -9,12 +9,14 @@ def driver():
     #b = 2
 
     f = lambda x: np.exp(x**2+7*x-30)-1
+    fp = lambda x: (np.exp(x**2+7*x-30))*(2*x+7)
+    fpp = lambda x: (np.exp(x**2+7*x-30))*(4*x**2+28*x+51)
     a = 2
     b = 4.5
 
-    tol = 1e-5
+    tol = 1e-10
 
-    [astar,ier] = bisection(f,a,b,tol)
+    [astar,ier] = bisection(f,fp,fpp,a,b,tol)
     print('the approximate root is',astar)
     print('the error message reads:',ier)
     print('f(astar) =', f(astar))
@@ -23,7 +25,7 @@ def driver():
 
 
 # define routines
-def bisection(f,a,b,tol):
+def bisection(f,fp,fpp,a,b,tol):
     
 #    Inputs:
 #     f,a,b       - function and endpoints of initial interval
@@ -57,7 +59,8 @@ def bisection(f,a,b,tol):
 
     count = 0
     d = 0.5*(a+b)
-    while (abs(d-a)> tol):
+    gp = (f(d)*fp(d))/(fpp(d)**2)
+    while (gp<1):
       fd = f(d)
       if (fd ==0):
         astar = d
@@ -69,13 +72,47 @@ def bisection(f,a,b,tol):
         a = d
         fa = fd
       d = 0.5*(a+b)
+      gp = (f(d)*fp(d))/(fpp(d)**2)
       count = count +1
+      print(count)
 #      print('abs(d-a) = ', abs(d-a))
-    print(count)
-      
-    astar = d
-    ier = 0
-    return [astar, ier]
-      
-driver()               
 
+    Nmax = 1000  
+    [p,pstar,info,it] = newton(f,fp,d,tol,Nmax)
+    
+    astar = pstar
+    ier = info
+    return [astar, ier]
+    
+def newton(f,fp,p0,tol,Nmax):
+  """
+  Newton iteration.
+  
+  Inputs:
+    f,fp - function and derivative
+    p0   - initial guess for root
+    tol  - iteration stops when p_n,p_{n+1} are within tol
+    Nmax - max number of iterations
+  Returns:
+    p     - an array of the iterates
+    pstar - the last iterate
+    info  - success message
+          - 0 if we met tol
+          - 1 if we hit Nmax iterations (fail)
+     
+  """
+  p = np.zeros(Nmax+1);
+  p[0] = p0
+  for it in range(Nmax):
+      p1 = p0-f(p0)/fp(p0)
+      p[it+1] = p1
+      if (abs(p1-p0) < tol):
+          pstar = p1
+          info = 0
+          return [p,pstar,info,it]
+      p0 = p1
+  pstar = p1
+  info = 1
+  return [p,pstar,info,it]
+      
+driver() 
